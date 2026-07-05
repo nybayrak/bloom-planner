25c610568acabaa9972c28fb7f36e87cb97a1695
nybayrak <151108121+nybayrak@users.noreply.github.com>
2026-07-04 22:43:24 -0400

    Remove Firebase initialization from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'core/db/isar_service.dart';
 import 'core/notifications/notification_service.dart';
 import 'core/router/router.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   await IsarService.init();
   await NotificationService.init();
   runApp(const ProviderScope(child: BloomPlannerApp()));
