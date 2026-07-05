646711ef375c9d6c895b887fc5afd91ed5d228c9
nybayrak <nybayrak@users.noreply.github.com>
2026-07-05 18:52:45 +0000

    deploy: 096f41ecc13885098ea43d4235c774c7edde2681

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "95676768"
+    serviceWorkerVersion: "3167862351"
   }
 });
